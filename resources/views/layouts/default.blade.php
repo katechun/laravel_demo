496d60fdf788897e689d330b774c9e63915624ec
katechun <875381900@qq.com>
2020-03-18 08:03:56 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title','欢迎到来') - Demo App</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('content')
+@section('title','主页')
+    <h1>主页</h1>
+@stop
+
+
